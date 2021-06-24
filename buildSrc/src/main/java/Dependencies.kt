7e1857f1722f5d0e79221791988191136184eb53
David Tverdota <76949181+dtverdota-bc@users.noreply.github.com>
2021-06-24 18:33:13 +0100

    chore(proguard): AND-0000 Fix Proguard (#2082)
    * Adding exceptions to proguard
    
    * Version bump

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -7,7 +7,7 @@ object Versions {
     const val targetSdk = 30
     const val compileSdk = 30
 
-    const val versionCode = 649
+    const val versionCode = 650
     const val versionName = "8.8.3"
     const val buildTools = "29.0.2"
 
