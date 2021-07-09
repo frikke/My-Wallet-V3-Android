df9b742ab208a2589ca9eb5773527b122d2f7375
David Tverdota <76949181+dtverdota-bc@users.noreply.github.com>
2021-07-09 08:17:01 +0100

    chore(modern_wallet_auth): AND-0000 Merge MWA Fixes (#2124)
    * fix(modern_wallet_auth): AND-0000 Fix MWA IP Comparison (#2121)
    
    * Fix serialization
    
    * Fix IP handling
    
    * Passing the correct IP
    
    * ktlint
    
    * Bump versions

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -7,8 +7,8 @@ object Versions {
     const val targetSdk = 30
     const val compileSdk = 30
 
-    const val versionCode = 660
-    const val versionName = "8.8.6"
+    const val versionCode = 661
+    const val versionName = "8.8.7"
     const val buildTools = "29.0.2"
 
     // Build tools and languages
