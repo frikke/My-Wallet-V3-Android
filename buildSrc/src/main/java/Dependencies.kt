3497f4379ed48bd22292284ff5dd6d573ec4bc89
Antonis Latas <antonis@blockchain.com>
2021-07-14 12:05:29 +0300

    update version code

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -7,7 +7,7 @@ object Versions {
     const val targetSdk = 30
     const val compileSdk = 30
 
-    const val versionCode = 662
+    const val versionCode = 663
     const val versionName = "8.8.7"
     const val buildTools = "29.0.2"
 
