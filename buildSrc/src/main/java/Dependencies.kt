cb87d3cf6184e0f911c59ee37195d42c5fc5ba64
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2021-08-16 10:35:24 +0000

    chore(release): AND-0000 Version Code Bump: 15428

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -7,7 +7,7 @@ object Versions {
     const val targetSdk = 30
     const val compileSdk = 30
 
-    const val versionCode = 15401
+    const val versionCode = 15428
     const val versionName = "8.10.2"
     const val buildTools = "29.0.2"
 
