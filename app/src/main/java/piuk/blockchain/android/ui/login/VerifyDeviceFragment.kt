ae97007adf4d6f4e41b44c165d90794d28f0ef36
dserrano-bc <64006245+dserrano-bc@users.noreply.github.com>
2021-09-07 16:03:13 +0200

    resend email (#2336)

--- app/src/main/java/piuk/blockchain/android/ui/login/VerifyDeviceFragment.kt
+++ app/src/main/java/piuk/blockchain/android/ui/login/VerifyDeviceFragment.kt
@@ -2,22 +2,37 @@ package piuk.blockchain.android.ui.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.Toast
+import com.blockchain.koin.scopedInject
 import piuk.blockchain.android.R
 import piuk.blockchain.android.databinding.FragmentVerifyDeviceBinding
+import piuk.blockchain.android.ui.base.mvi.MviFragment
+import piuk.blockchain.android.ui.customviews.ToastCustom
+import java.util.concurrent.atomic.AtomicBoolean
 
-class VerifyDeviceFragment : Fragment() {
+class VerifyDeviceFragment : MviFragment<LoginModel, LoginIntents, LoginState, FragmentVerifyDeviceBinding>() {
 
-    private var _binding: FragmentVerifyDeviceBinding? = null
-    private val binding: FragmentVerifyDeviceBinding
-        get() = _binding!!
+    override val model: LoginModel by scopedInject()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        _binding = FragmentVerifyDeviceBinding.inflate(inflater, container, false)
-        return binding.root
+    override fun initBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentVerifyDeviceBinding =
+        FragmentVerifyDeviceBinding.inflate(inflater, container, false)
+
+    private lateinit var state: LoginState
+
+    private val isTimerRunning = AtomicBoolean(false)
+    private val timer = object : CountDownTimer(RESEND_TIMEOUT, TIMER_STEP) {
+        override fun onTick(millisUntilFinished: Long) {
+            isTimerRunning.set(true)
+        }
+
+        override fun onFinish() {
+            isTimerRunning.set(false)
+            binding.resendEmailButton.isActivated = true
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -34,11 +49,30 @@ class VerifyDeviceFragment : Fragment() {
                     startActivity(Intent.createChooser(this, getString(R.string.security_centre_email_check)))
                 }
             }
+            resendEmailButton.setOnClickListener {
+                if (!isTimerRunning.get()) {
+                    timer.start()
+                    model.process(LoginIntents.SendEmail(state.sessionId, state.email, state.captcha))
+                    ToastCustom.makeText(
+                        requireContext(), getString(R.string.verify_device_email_resent), Toast.LENGTH_SHORT,
+                        ToastCustom.TYPE_OK
+                    )
+                } else {
+                    ToastCustom.makeText(
+                        requireContext(), getString(R.string.verify_device_resend_blocked), Toast.LENGTH_LONG,
+                        ToastCustom.TYPE_ERROR
+                    )
+                }
+            }
         }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun render(newState: LoginState) {
+        state = newState
+    }
+
+    companion object {
+        private const val RESEND_TIMEOUT = 30000L
+        private const val TIMER_STEP = 1000L
     }
 }
