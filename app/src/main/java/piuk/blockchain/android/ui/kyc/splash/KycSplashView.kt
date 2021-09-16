7d5f4cc8eb82e499fef7d6d154f0024eb1fe493e
antonis-bc <49150635+antonis-bc@users.noreply.github.com>
2021-09-16 18:46:57 +0300

    fix(kyc): AND-4696 Put splash back (#2365)

---
+++ app/src/main/java/piuk/blockchain/android/ui/kyc/splash/KycSplashView.kt
@@ -0,0 +1,14 @@
+package piuk.blockchain.android.ui.kyc.splash
+
+import androidx.navigation.NavDirections
+import piuk.blockchain.android.ui.base.View
+
+interface KycSplashView : View {
+    fun displayLoading(isLoading: Boolean)
+
+    fun goToNextKycStep(direction: NavDirections)
+
+    fun showError(message: String)
+
+    fun onEmailNotVerified()
+}
