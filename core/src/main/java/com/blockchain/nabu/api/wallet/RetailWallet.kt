ff02c7dd79c34cc6296c95e98a944f580f566dc9
André Romano <91881450+aromano-bc@users.noreply.github.com>
2021-10-12 15:37:51 +0100

    fix(update_requestjwt_endpoint): Updated request JWT endpoint from old GET to new POST method (#2454)
    * Changed requestJwt request from outdated GET request to new POST

--- core/src/main/java/com/blockchain/nabu/api/wallet/RetailWallet.kt
+++ core/src/main/java/com/blockchain/nabu/api/wallet/RetailWallet.kt
@@ -2,17 +2,20 @@ package com.blockchain.nabu.api.wallet
 
 import com.blockchain.nabu.models.responses.wallet.RetailJwtResponse
 import io.reactivex.rxjava3.core.Single
-import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
 import retrofit2.http.Url
 
 internal interface RetailWallet {
 
-    @GET
+    @FormUrlEncoded
+    @POST
     fun requestJwt(
         @Url url: String,
-        @Query("guid") guid: String,
-        @Query("sharedKey") sharedKey: String,
-        @Query("api_code") apiCode: String
+        @Field("guid") guid: String,
+        @Field("sharedKey") sharedKey: String,
+        @Field("api_code") apiCode: String,
+        @Field("method") method: String = RETAIL_JWT_TOKEN_METHOD
     ): Single<RetailJwtResponse>
 }
--- core/src/main/java/com/blockchain/nabu/service/RetailWalletTokenService.kt
+++ core/src/main/java/com/blockchain/nabu/service/RetailWalletTokenService.kt
@@ -1,6 +1,6 @@
 package com.blockchain.nabu.service
 
-import com.blockchain.nabu.api.wallet.RETAIL_JWT_TOKEN
+import com.blockchain.nabu.api.wallet.RETAIL_JWT_TOKEN_PATH
 import com.blockchain.nabu.api.wallet.RetailWallet
 import com.blockchain.nabu.models.responses.wallet.RetailJwtResponse
 import io.reactivex.rxjava3.core.Single
@@ -15,7 +15,7 @@ class RetailWalletTokenService(
     private val service: RetailWallet = retrofit.create(RetailWallet::class.java)
 
     internal fun requestJwt(
-        path: String = explorerPath + RETAIL_JWT_TOKEN,
+        path: String = explorerPath + RETAIL_JWT_TOKEN_PATH,
         guid: String,
         sharedKey: String
     ): Single<RetailJwtResponse> = service.requestJwt(
