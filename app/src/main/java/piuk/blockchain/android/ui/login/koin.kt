5a3bedecb5bc3b355b42de20c5d324cd49d57c7c
André Romano <91881450+aromano-bc@users.noreply.github.com>
2021-11-04 12:22:02 +0000

    changed from LoginModel from scoped back to factory so the activity gets a fresh one everytime (#2522)

--- app/src/main/java/piuk/blockchain/android/ui/login/koin.kt
+++ app/src/main/java/piuk/blockchain/android/ui/login/koin.kt
@@ -11,7 +11,7 @@ import piuk.blockchain.android.ui.login.auth.LoginAuthState
 val loginUiModule = module {
 
     scope(payloadScopeQualifier) {
-        scoped {
+        factory {
             LoginModel(
                 initialState = LoginState(),
                 mainScheduler = AndroidSchedulers.mainThread(),
