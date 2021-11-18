f9726d8495d6f584e0c99a81ce505f451f170dae
slin-bc <87092976+slin-bc@users.noreply.github.com>
2021-11-18 12:50:36 -0500

    Update bottomnav design (#2608)

--- componentlib/src/main/java/com/blockchain/componentlib/navigation/BottomNavigationBar.kt
+++ componentlib/src/main/java/com/blockchain/componentlib/navigation/BottomNavigationBar.kt
@@ -7,30 +7,43 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.absoluteOffset
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.blockchain.componentlib.R
 import com.blockchain.componentlib.theme.AppTheme
 import com.blockchain.componentlib.theme.Dark400
+import com.blockchain.componentlib.theme.Dark700
 import com.blockchain.componentlib.theme.Dark900
+import com.blockchain.componentlib.theme.Grey000
 import com.blockchain.componentlib.theme.Grey400
 import com.blockchain.componentlib.theme.Grey600
 
@@ -72,61 +85,108 @@ fun BottomNavigationBar(
         Grey600
     }
 
+    val dividerColor = if (!isSystemInDarkTheme()) {
+        Grey000
+    } else {
+        Dark700
+    }
+
     BottomNavigation(
         backgroundColor = backgroundColor
     ) {
-        navigationItems.forEachIndexed { index, item ->
-            if (index == middleIndex) {
-                Box(
-                    modifier = Modifier.clickable(
-                        interactionSource = remember { MutableInteractionSource() },
-                        indication = null
-                    ) {
-                        onMiddleButtonClick.invoke()
-                    }
-                ) {
-                    Crossfade(targetState = bottomNavigationState) { state ->
-                        when (state) {
-                            BottomNavigationState.Add -> Image(
-                                painter = painterResource(R.drawable.ic_bottom_nav_add),
-                                contentDescription = null
-                            )
-                            BottomNavigationState.Cancel -> Image(
-                                painter = painterResource(R.drawable.ic_bottom_nav_cancel),
-                                contentDescription = null
+        Column {
+            Divider(thickness = 1.dp, color = dividerColor, modifier = Modifier.fillMaxWidth())
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+                navigationItems.forEachIndexed { index, item ->
+                    if (index == middleIndex) {
+                        Box(
+                            modifier = Modifier.clickable(
+                                interactionSource = remember { MutableInteractionSource() },
+                                indication = null
+                            ) {
+                                onMiddleButtonClick.invoke()
+                            }
+                        ) {
+                            Crossfade(targetState = bottomNavigationState) { state ->
+                                when (state) {
+                                    BottomNavigationState.Add -> Image(
+                                        painter = painterResource(R.drawable.ic_bottom_nav_add),
+                                        contentDescription = null
+                                    )
+                                    BottomNavigationState.Cancel -> Image(
+                                        painter = painterResource(R.drawable.ic_bottom_nav_cancel),
+                                        contentDescription = null
+                                    )
+                                }
+                            }
+                            Image(
+                                modifier = Modifier
+                                    .height(56.dp)
+                                    .width(56.dp)
+                                    .padding(AppTheme.dimensions.paddingMedium)
+                                    .rotate(rotation),
+                                painter = painterResource(R.drawable.ic_bottom_nav_plus),
+                                contentDescription = null,
+                                colorFilter = ColorFilter.tint(backgroundColor)
                             )
                         }
                     }
-                    Image(
-                        modifier = Modifier
-                            .height(56.dp)
-                            .width(56.dp)
-                            .padding(16.dp)
-                            .rotate(rotation),
-                        painter = painterResource(R.drawable.ic_bottom_nav_plus),
-                        contentDescription = null,
-                        colorFilter = ColorFilter.tint(backgroundColor)
-                    )
+
+                    val selected = item == selectedNavigationItem
+                    BottomNavigationItemWithIndicator(selected = selected) {
+                        BottomNavigationItem(
+                            icon = {
+                                Icon(
+                                    painter = painterResource(id = item.icon),
+                                    contentDescription = stringResource(item.title)
+                                )
+                            },
+                            label = {
+                                Text(
+                                    maxLines = 1,
+                                    overflow = TextOverflow.Ellipsis,
+                                    text = stringResource(item.title),
+                                    fontSize = 10.sp,
+                                    color = if (selected) {
+                                        AppTheme.colors.primary
+                                    } else {
+                                        textColor
+                                    }
+                                )
+                            },
+                            selectedContentColor = selectedContentColor,
+                            unselectedContentColor = unselectedContentColor,
+                            alwaysShowLabel = true,
+                            selected = selected,
+                            onClick = {
+                                onNavigationItemClick(item)
+                            }
+                        )
+                    }
                 }
             }
-            BottomNavigationItem(
-                icon = { Icon(painterResource(id = item.icon), contentDescription = stringResource(item.title)) },
-                label = {
-                    Text(
-                        text = stringResource(item.title),
-                        fontSize = 10.sp,
-                        color = textColor
-                    )
-                },
-                selectedContentColor = selectedContentColor,
-                unselectedContentColor = unselectedContentColor,
-                alwaysShowLabel = true,
-                selected = item == selectedNavigationItem,
-                onClick = {
-                    onNavigationItemClick(item)
-                }
+        }
+    }
+}
+
+@Composable
+fun BottomNavigationItemWithIndicator(
+    selected: Boolean,
+    BottomNavigationItem: @Composable () -> Unit
+) {
+    Column(modifier = Modifier.width(IntrinsicSize.Max), horizontalAlignment = Alignment.CenterHorizontally) {
+        if (selected) {
+            Divider(
+                modifier = Modifier
+                    .clipToBounds()
+                    .absoluteOffset(y = (-2).dp)
+                    .fillMaxWidth(0.8f)
+                    .clip(AppTheme.shapes.small),
+                thickness = 4.dp,
+                color = AppTheme.colors.primary
             )
         }
+        BottomNavigationItem()
     }
 }
 
@@ -138,7 +198,7 @@ enum class BottomNavigationState {
 @Composable
 fun BottomNavigationBarPreview() {
     AppTheme {
-        BottomNavigationBar()
+        BottomNavigationBar(selectedNavigationItem = NavigationItem.Home)
     }
 }
 
