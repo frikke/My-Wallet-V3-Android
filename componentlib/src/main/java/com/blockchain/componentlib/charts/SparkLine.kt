c6550411055448f91961c2953df6a0948f52fcd1
Arjun Achatz <arjunachatz@gmail.com>
2021-12-01 16:29:43 -0500

    Moving to path

--- componentlib/src/main/java/com/blockchain/componentlib/charts/SparkLine.kt
+++ componentlib/src/main/java/com/blockchain/componentlib/charts/SparkLine.kt
@@ -4,73 +4,86 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.blockchain.componentlib.theme.AppSurface
 import com.blockchain.componentlib.theme.AppTheme
-import com.blockchain.componentlib.theme.Blue400
-import com.blockchain.componentlib.theme.Red400
 
 @Composable
 fun SparkLine(
-    historicalRate: List<SparkLineHistoricalRate> = data,
+    historicalRate: List<SparkLineHistoricalRate>,
     modifier: Modifier = Modifier,
 ) {
+    if (historicalRate.isEmpty()) {
+        return
+    }
+
+    // Assumption : Crypto rates are never negative
+    val maxRate by remember(historicalRate) {
+        mutableStateOf(
+            historicalRate.maxByOrNull(SparkLineHistoricalRate::rate) ?: return
+        )
+    }
+
     val strokeColor = AppTheme.colors.primary
 
     Canvas(modifier.background(Color.White)) {
 
         val height = this.size.height
         val width = this.size.width
-
-        val interval = width / (historicalRate.size + 2)
+        val interval = width / historicalRate.size
         var currentX = interval
-        val halfHeight = height / 2f
-
+        val rateScalerValue = height / maxRate.rate.toFloat()
         val gradient = Brush.horizontalGradient(
             colors = listOf(Color.Transparent, strokeColor),
             startX = 0.0f,
-            endX = width,
+            endX = width / 2f,
         )
 
+        val path = Path()
+
+        path.moveTo(0f, historicalRate.first().rate.toFloat() * rateScalerValue)
+
         historicalRate.forEachIndexed { index, rateEntry ->
-            val nextRateEntry = historicalRate.getOrNull(index + 1) ?: return@forEachIndexed
-            drawLine(
-                brush = gradient,
-                strokeWidth = Stroke.DefaultMiter,
-                start = Offset(currentX, halfHeight + rateEntry.rate.toFloat()),
-                end = Offset(currentX + interval, halfHeight + nextRateEntry.rate.toFloat()),
-            )
+            if (index == 0) {
+                return@forEachIndexed
+            }
+            path.lineTo(currentX, rateEntry.rate.toFloat() * rateScalerValue)
             currentX += interval
         }
-    }
-}
 
-val data: List<SparkLineHistoricalRate> = List(20) {
-    object : SparkLineHistoricalRate {
-        override val timestamp: Long = it.toLong()
-        override val rate: Double = Math.random() * 16
+        drawPath(
+            path = path,
+            brush = gradient,
+            style = Stroke(width = 2.dp.toPx()),
+        )
     }
-}
-
-interface SparkLineHistoricalRate {
-    val timestamp: Long
-    val rate: Double
 }
 
 @Preview
 @Composable
 fun SparkLinePreview() {
+    val data: List<SparkLineHistoricalRate> = List(20) {
+        object : SparkLineHistoricalRate {
+            override val timestamp: Long = it.toLong()
+            override val rate: Double = Math.random() * 1000
+        }
+    }
+
     AppTheme {
         AppSurface {
-            SparkLine(modifier = Modifier.size(64.dp, 16.dp))
+            SparkLine(
+                historicalRate = data,
+                modifier = Modifier.size(64.dp, 16.dp)
+            )
         }
     }
 }
