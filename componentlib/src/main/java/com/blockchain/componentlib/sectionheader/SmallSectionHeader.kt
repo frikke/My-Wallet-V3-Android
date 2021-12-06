fb5e1ca5d5d2a8aba6dea1daee7d3960ac2df5a8
Arjun Achatz <arjunachatz@gmail.com>
2021-12-06 15:13:17 -0500

    ktlint + section header

--- componentlib/src/main/java/com/blockchain/componentlib/sectionheader/SmallSectionHeader.kt
+++ componentlib/src/main/java/com/blockchain/componentlib/sectionheader/SmallSectionHeader.kt
@@ -10,7 +10,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.UiMode
 import com.blockchain.componentlib.theme.AppSurface
 import com.blockchain.componentlib.theme.AppTheme
 
