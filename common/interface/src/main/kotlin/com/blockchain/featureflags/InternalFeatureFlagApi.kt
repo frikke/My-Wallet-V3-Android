f2d4606c993a542db9f8ea62c7551140a212ad80
David Tverdota <76949181+dtverdota-bc@users.noreply.github.com>
2021-12-14 16:17:17 +0000

    fix(payments): AND-0000 Disable Quotes FF in Prod (#2749)

--- common/interface/src/main/kotlin/com/blockchain/featureflags/InternalFeatureFlagApi.kt
+++ common/interface/src/main/kotlin/com/blockchain/featureflags/InternalFeatureFlagApi.kt
@@ -14,7 +14,7 @@ enum class GatedFeature(
 ) {
     ADD_SUB_WALLET_ADDRESSES("Create BTC sub-wallets"),
     LANDING_CTA("Enable Landing CTA Screen"),
-    NEW_PRICING_BROKERAGE_QUOTE("New pricing quote api", true),
+    NEW_PRICING_BROKERAGE_QUOTE("New pricing quote api", false),
     FAB_SHEET_CTAS("Show Buy on RHS and Sell on LHS in the FAB bottom sheet", true),
     REDESIGN_PT2("Enable Redesign part 2", false)
 }
