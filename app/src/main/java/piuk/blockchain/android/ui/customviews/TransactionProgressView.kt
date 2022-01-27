93be8c2495371e98e4da357855a7e2389e6ff864
lmiguelez-bc <82380200+lmiguelez-bc@users.noreply.github.com>
2022-01-27 17:35:35 +0000

    conflict

--- app/src/main/java/piuk/blockchain/android/ui/customviews/TransactionProgressView.kt
+++ app/src/main/java/piuk/blockchain/android/ui/customviews/TransactionProgressView.kt
@@ -99,7 +99,11 @@ class TransactionProgressView(context: Context, attrs: AttributeSet) :
         }
     }
 
-    fun showTxError(title: String, subtitle: CharSequence, resourceIcon: Int = R.drawable.ic_alert_logo) {
+    fun showTxError(
+        title: String,
+        subtitle: CharSequence,
+        resourceIcon: Int = R.drawable.ic_alert_white_bkgd
+    ) {
         with(binding) {
             txStateIndicator.setImageResource(resourceIcon)
             txStateIndicator.visible()
