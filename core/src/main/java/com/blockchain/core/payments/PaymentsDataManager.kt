96a3133f42c18ea2825d082003a192ab6cd8db29
dserrano-bc <64006245+dserrano-bc@users.noreply.github.com>
2022-02-01 18:12:24 +0100

    signed commit (#2862)

--- core/src/main/java/com/blockchain/core/payments/PaymentsDataManager.kt
+++ core/src/main/java/com/blockchain/core/payments/PaymentsDataManager.kt
@@ -45,6 +45,7 @@ import com.blockchain.core.payments.model.LinkedBankState
 import com.blockchain.core.payments.model.Partner
 import com.blockchain.core.payments.model.PartnerCredentials
 import com.blockchain.core.payments.model.PaymentMethodDetailsError
+import com.blockchain.extensions.wrapErrorMessage
 import com.blockchain.nabu.datamanagers.BillingAddress
 import com.blockchain.nabu.datamanagers.PaymentLimits
 import com.blockchain.nabu.datamanagers.PaymentMethod
@@ -345,7 +346,7 @@ class PaymentsDataManagerImpl(
             }
         }.also {
             cardsCache.invalidate()
-        }
+        }.wrapErrorMessage()
 
     override fun getCardDetails(cardId: String): Single<PaymentMethod.Card> =
         authenticator.getAuthHeader().flatMap { authToken ->
