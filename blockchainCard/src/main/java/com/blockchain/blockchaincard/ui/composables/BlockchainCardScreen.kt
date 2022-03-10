ed9032853d0d563c146fee3496ed891bb3117f69
labreu-bc <labreu@blockchain.com>
2022-03-10 18:49:27 +0000

    Add composables for Order Card Intro and Order Card Selection

--- blockchainCard/src/main/java/com/blockchain/blockchaincard/ui/composables/BlockchainCardScreen.kt
+++ blockchainCard/src/main/java/com/blockchain/blockchaincard/ui/composables/BlockchainCardScreen.kt
@@ -1,20 +1,16 @@
 package com.blockchain.blockchaincard.ui.composables
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,46 +18,62 @@ import com.blockchain.blockchaincard.R
 import com.blockchain.componentlib.basic.ComposeColors
 import com.blockchain.componentlib.basic.ComposeGravities
 import com.blockchain.componentlib.basic.ComposeTypographies
+import com.blockchain.componentlib.basic.SimpleImageView
 import com.blockchain.componentlib.basic.SimpleText
-import com.blockchain.componentlib.theme.AppDimensions
+import com.blockchain.componentlib.button.MinimalButton
+import com.blockchain.componentlib.button.PrimaryButton
 import com.blockchain.componentlib.theme.AppSurface
 import com.blockchain.componentlib.theme.AppTheme
 
 @Composable
 fun BlockchainCardScreen() {
-    OrderCardScreen()
+    OrderOrLinkCard({},{})
 }
 
 @Composable
-private fun OrderCardScreen() {
+private fun OrderOrLinkCard(
+    onOrderCard: () -> Unit,
+    onLinkCard: () -> Unit
+) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(AppTheme.dimensions.paddingLarge)
     ) {
         Image(
             painter = painterResource(id = R.drawable.ic_graphic_cards),
             contentDescription = "Blockchain Card",
             modifier = Modifier.padding(0.dp, AppTheme.dimensions.xxxPaddingLarge, AppTheme.dimensions.paddingMedium, 0.dp)
         )
+
         SimpleText(
             text = "Your Gateway To The Blockchain Debit Card",
             style = ComposeTypographies.Title2,
             color = ComposeColors.Title,
             gravity = ComposeGravities.Centre
         )
+
         SimpleText(
             text = "A card that lets you spend and earn in crypto right from your Blockchain account.",
             style = ComposeTypographies.Paragraph1,
             color = ComposeColors.Body,
             gravity = ComposeGravities.Centre
         )
+
         Spacer(Modifier.size(115.dp))
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Order My Card", color = Color.White, modifier = Modifier.background(color = Color.Blue, shape = RoundedCornerShape(3.dp)))
-        }
+
+        PrimaryButton(
+            text = "Order My Card",
+            onClick = onOrderCard,
+            modifier = Modifier.fillMaxWidth()
+        )
+
         Spacer(Modifier.size(AppTheme.dimensions.paddingSmall))
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Already Have A Card? Link It Here", color = Color.Blue,)
-        }
+
+        MinimalButton(
+            text = "Already Have A Card? Link It Here",
+            onClick = onLinkCard,
+            modifier = Modifier.fillMaxWidth()
+        )
     }
 }
 
@@ -70,7 +82,57 @@ private fun OrderCardScreen() {
 private fun PreviewOrderCardScreen() {
     AppTheme(darkTheme = false) {
         AppSurface {
-            OrderCardScreen()
+            OrderOrLinkCard({},{})
         }
     }
 }
+
+@Composable
+private fun SelectCardForOrder() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(AppTheme.dimensions.paddingLarge)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_card),
+                contentDescription = "Blockchain Card",
+                modifier = Modifier.padding(0.dp, AppTheme.dimensions.paddingLarge, AppTheme.dimensions.paddingMedium, 0.dp)
+            )
+
+            SimpleText(
+                text = "Virtual",
+                style = ComposeTypographies.Title2,
+                color = ComposeColors.Title,
+                gravity = ComposeGravities.Centre
+            )
+
+            SimpleText(
+                text = "Our digital only card, use instantly for online payments. ",
+                style = ComposeTypographies.Paragraph1,
+                color = ComposeColors.Body,
+                gravity = ComposeGravities.Centre
+            )
+        }
+
+        PrimaryButton(
+            text = "Create Card",
+            onClick = { },
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .padding(AppTheme.dimensions.paddingLarge)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewSelectCardForOrder() {
+    AppTheme(darkTheme = false) {
+        AppSurface {
+            SelectCardForOrder()
+        }
+    }
+}
+
