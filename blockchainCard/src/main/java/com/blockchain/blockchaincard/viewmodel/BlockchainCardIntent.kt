28f70bfb169271d01d63512790e82c7812cf28da
labreu-bc <labreu@blockchain.com>
2022-03-22 14:04:11 +0000

    Update new mvi Intent interface implementation

--- blockchainCard/src/main/java/com/blockchain/blockchaincard/viewmodel/BlockchainCardIntent.kt
+++ blockchainCard/src/main/java/com/blockchain/blockchaincard/viewmodel/BlockchainCardIntent.kt
@@ -2,7 +2,7 @@ package com.blockchain.blockchaincard.viewmodel
 
 import com.blockchain.commonarch.presentation.mvi_v2.Intent
 
-sealed class BlockchainCardIntent : Intent {
+sealed class BlockchainCardIntent : Intent<BlockchainCardModelState> {
     object OrderCard : BlockchainCardIntent()
     object LinkCard : BlockchainCardIntent()
     object ManageCard : BlockchainCardIntent()
