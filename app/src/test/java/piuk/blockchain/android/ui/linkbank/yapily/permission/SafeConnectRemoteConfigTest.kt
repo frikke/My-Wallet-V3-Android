18157efa8a6f5ca93fd16b84b3d2688a5ed45882
Othman El Jazouli <oteljazouli@blockchain.com>
2022-04-28 00:38:19 +0000

    [FEAT - AND-5994 - Remove Safeconnect Screens] unit tests

---
+++ app/src/test/java/piuk/blockchain/android/ui/linkbank/yapily/permission/SafeConnectRemoteConfigTest.kt
@@ -0,0 +1,30 @@
+package piuk.blockchain.android.ui.linkbank.yapily.permission
+
+import com.blockchain.remoteconfig.RemoteConfig
+import io.mockk.every
+import io.mockk.mockk
+import io.reactivex.rxjava3.core.Single
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+
+@ExperimentalCoroutinesApi
+class SafeConnectRemoteConfigTest {
+    private val remoteConfig = mockk<RemoteConfig>()
+    private val safeConnectRemoteConfig = SafeConnectRemoteConfig(remoteConfig)
+
+    private val rawString = "rawString"
+
+    @Before
+    fun setUp() {
+        every { remoteConfig.getRawJson(any()) } returns Single.just(rawString)
+    }
+
+    @Test
+    fun `WHEN remoteConfig returns, THEN getTosPdfLink should return that raw value`() = runTest {
+        val result = safeConnectRemoteConfig.getTosPdfLink()
+        assertEquals(rawString, result)
+    }
+}
---
+++ app/src/test/java/piuk/blockchain/android/ui/linkbank/yapily/permission/YapilyPermissionViewModelTest.kt
@@ -0,0 +1,68 @@
+package piuk.blockchain.android.ui.linkbank.yapily.permission
+
+import app.cash.turbine.test
+import com.blockchain.outcome.Outcome
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import piuk.blockchain.android.fileutils.domain.usecase.DownloadFileUseCase
+import java.io.File
+import kotlin.test.assertEquals
+
+@ExperimentalCoroutinesApi
+class YapilyPermissionViewModelTest {
+    private val safeConnectRemoteConfig = mockk<SafeConnectRemoteConfig>()
+    private val downloadFileUseCase = mockk<DownloadFileUseCase>()
+
+    private lateinit var viewModel: YapilyPermissionViewModel
+
+    private val path = "absolute/path/to/pdf"
+    private val link = "fb/link/to/pdf"
+    private val data = Outcome.Success(mockk<File>())
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+
+        viewModel = YapilyPermissionViewModel(
+            safeConnectRemoteConfig,
+            downloadFileUseCase
+        )
+
+        coEvery { safeConnectRemoteConfig.getTosPdfLink() } returns link
+        coEvery { downloadFileUseCase(any(), any()) } returns data
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `WHEN DownloadTermsOfService is triggered, THEN downloadFileUseCase should be called`() =
+        runTest {
+            viewModel.onIntent(YapilyPermissionIntents.DownloadTermsOfService(path))
+
+            coVerify(exactly = 1) { downloadFileUseCase(absolutePath = path, fileGsLink = link) }
+        }
+
+    @Test
+    fun `WHEN downloadFileUseCase returns, THEN OpenFile should be triggered`() =
+        runTest {
+            viewModel.navigationEventFlow.test {
+                viewModel.onIntent(YapilyPermissionIntents.DownloadTermsOfService(path))
+
+                val expected = YapilyPermissionNavigationEvent.OpenFile(data.value)
+                assertEquals(expected, expectMostRecentItem())
+            }
+        }
+}
