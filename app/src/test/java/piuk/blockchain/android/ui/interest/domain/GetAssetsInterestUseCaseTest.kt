00f9378329af5b0217180e788750578ab3c20aa3
Othman El Jazouli <oteljazouli@blockchain.com>
2022-04-28 09:45:06 +0000

    [AND-5925 - order rewards screen] remove (working) test to check bitrise failure

--- app/src/test/java/piuk/blockchain/android/ui/interest/domain/GetAssetsInterestUseCaseTest.kt
+++ app/src/test/java/piuk/blockchain/android/ui/interest/domain/GetAssetsInterestUseCaseTest.kt
@@ -1,12 +1,16 @@
 package piuk.blockchain.android.ui.interest.domain
 
 import com.blockchain.outcome.Outcome
+import info.blockchain.balance.CryptoCurrency
+import info.blockchain.balance.Money
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlin.test.assertTrue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
+import piuk.blockchain.android.ui.interest.domain.model.AssetInterestDetail
+import piuk.blockchain.android.ui.interest.domain.model.InterestAsset
 import piuk.blockchain.android.ui.interest.domain.repository.AssetInterestService
 import piuk.blockchain.android.ui.interest.domain.usecase.GetAssetsInterestUseCase
 
@@ -24,54 +28,57 @@ class GetAssetsInterestUseCaseTest {
 
     @Test
     fun `WHEN service returns mixed data, THEN filter before returning`() = runTest {
+
+        val list = listOf(
+            InterestAsset(
+                CryptoCurrency.ETHER,
+                AssetInterestDetail(
+                    mockk(), mockk(), mockk(relaxed = true), mockk(relaxed = true), mockk(),
+                    totalBalanceFiat = Money.fromMajor(CryptoCurrency.ETHER, 100.toBigDecimal())
+                )
+            ),
+            InterestAsset(
+                CryptoCurrency.BTC,
+                AssetInterestDetail(
+                    mockk(), mockk(), mockk(relaxed = true), mockk(relaxed = true), mockk(),
+                    totalBalanceFiat = Money.fromMajor(CryptoCurrency.BTC, 0.toBigDecimal())
+                )
+            ),
+            InterestAsset(
+                CryptoCurrency.XLM,
+                AssetInterestDetail(
+                    mockk(), mockk(), mockk(relaxed = true), mockk(relaxed = true), mockk(),
+                    totalBalanceFiat = Money.fromMajor(CryptoCurrency.XLM, 200.toBigDecimal())
+                )
+            ),
+            InterestAsset(
+                CryptoCurrency.BCH,
+                AssetInterestDetail(
+                    mockk(), mockk(), mockk(relaxed = true), mockk(relaxed = true), mockk(),
+                    totalBalanceFiat = Money.fromMajor(CryptoCurrency.XLM, 0.toBigDecimal())
+                )
+            )
+        )
+
         coEvery { service.getAssetsInterest(any()) } returns Outcome.Success(listOf())
 
         assertTrue { useCase(listOf()) is Outcome.Success }
 
-//        val list = listOf(
-//            InterestAsset(
-//                CryptoCurrency.ETHER,
-//                AssetInterestDetail(
-//                    mockk(), mockk(), mockk(relaxed = true), mockk(relaxed = true), mockk(),
-//                    totalBalanceFiat = Money.fromMajor(CryptoCurrency.ETHER, 100.toBigDecimal())
-//                )
-//            ),
-//            InterestAsset(
-//                CryptoCurrency.BTC,
-//                AssetInterestDetail(
-//                    mockk(), mockk(), mockk(relaxed = true), mockk(relaxed = true), mockk(),
-//                    totalBalanceFiat = Money.fromMajor(CryptoCurrency.BTC, 0.toBigDecimal())
-//                )
-//            ),
-//            InterestAsset(
-//                CryptoCurrency.XLM,
-//                AssetInterestDetail(
-//                    mockk(), mockk(), mockk(relaxed = true), mockk(relaxed = true), mockk(),
-//                    totalBalanceFiat = Money.fromMajor(CryptoCurrency.XLM, 200.toBigDecimal())
-//                )
-//            ),
-//            InterestAsset(
-//                CryptoCurrency.BCH,
-//                AssetInterestDetail(
-//                    mockk(), mockk(), mockk(relaxed = true), mockk(relaxed = true), mockk(),
-//                    totalBalanceFiat = Money.fromMajor(CryptoCurrency.XLM, 0.toBigDecimal())
-//                )
-//            )
-//        )
+        //
         // sort should be xlm (higher balance) - eth (lower balance) - btc (balance 0 but has priority) - bch
-//        coEvery { service.getAssetsInterest(any()) } returns Outcome.Success(listOf())
-//
-//        // testing why bitrise fails here
-//
-//        val result = useCase(listOf())
-//        assertTrue { result is Outcome.Success }
+        //        coEvery { service.getAssetsInterest(any()) } returns Outcome.Success(listOf())
+        //
+        //        // testing why bitrise fails here
+        //
+        //        val result = useCase(listOf())
+        //        assertTrue { result is Outcome.Success }
 
-//        //
-//        (result as Outcome.Success).value.let { value ->
-//            assertEquals(CryptoCurrency.XLM, value[0].assetInfo)
-//            assertEquals(CryptoCurrency.ETHER, value[1].assetInfo)
-//            assertEquals(CryptoCurrency.BTC, value[2].assetInfo)
-//            assertEquals(CryptoCurrency.BCH, value[3].assetInfo)
-//        }
+        //        //
+        //        (result as Outcome.Success).value.let { value ->
+        //            assertEquals(CryptoCurrency.XLM, value[0].assetInfo)
+        //            assertEquals(CryptoCurrency.ETHER, value[1].assetInfo)
+        //            assertEquals(CryptoCurrency.BTC, value[2].assetInfo)
+        //            assertEquals(CryptoCurrency.BCH, value[3].assetInfo)
+        //        }
     }
 }
