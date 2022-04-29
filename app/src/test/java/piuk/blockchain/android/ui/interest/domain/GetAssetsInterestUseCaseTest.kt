c20a4164947c36cee17cafc41bf008711502651d
Othman El Jazouli <oteljazouli@blockchain.com>
2022-04-29 02:14:15 +0000

    [AND-5925 - order rewards screen] remove (working) test to check bitrise failure

--- app/src/test/java/piuk/blockchain/android/ui/interest/domain/GetAssetsInterestUseCaseTest.kt
+++ app/src/test/java/piuk/blockchain/android/ui/interest/domain/GetAssetsInterestUseCaseTest.kt
@@ -44,13 +44,13 @@ class GetAssetsInterestUseCaseTest {
                     totalBalanceFiat = Money.fromMajor(CryptoCurrency.BTC, 0.toBigDecimal())
                 )
             ),
-//            InterestAsset(
-//                CryptoCurrency.XLM,
-//                AssetInterestDetail(
-//                    mockk(), mockk(), mockk(relaxed = true), mockk(relaxed = true), mockk(),
-//                    totalBalanceFiat = Money.fromMajor(CryptoCurrency.XLM, 200.toBigDecimal())
-//                )
-//            ),
+            InterestAsset(
+                CryptoCurrency.XLM,
+                AssetInterestDetail(
+                    mockk(), mockk(), mockk(relaxed = true), mockk(relaxed = true), mockk(),
+                    totalBalanceFiat = Money.fromMajor(CryptoCurrency.XLM, 200.toBigDecimal())
+                )
+            ),
 //            InterestAsset(
 //                CryptoCurrency.BCH,
 //                AssetInterestDetail(
