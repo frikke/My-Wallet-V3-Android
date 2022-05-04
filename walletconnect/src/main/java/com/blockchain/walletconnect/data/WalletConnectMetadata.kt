877cacc0ccc692289cb7e088ad3e3acca93c1387
rscrieciu-bc <95220871+rscrieciu-bc@users.noreply.github.com>
2022-05-04 13:44:46 +0100

    Handling missing sessions in WalletConnectMetadata (#3221)

--- walletconnect/src/main/java/com/blockchain/walletconnect/data/WalletConnectMetadata.kt
+++ walletconnect/src/main/java/com/blockchain/walletconnect/data/WalletConnectMetadata.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class WalletConnectMetadata(
-    val sessions: WalletConnectSessions
+    val sessions: WalletConnectSessions?
 )
 
 @Serializable
