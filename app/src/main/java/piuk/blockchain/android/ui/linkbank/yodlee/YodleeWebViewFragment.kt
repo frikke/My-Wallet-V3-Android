5b55b9013f084d0ddd99b634598bdcf6648fb1f0
antonis-bc <49150635+antonis-bc@users.noreply.github.com>
2022-05-16 19:20:20 +0300

    fix(ach): AND-0000 ACH deeplinking (#3295)

--- app/src/main/java/piuk/blockchain/android/ui/linkbank/yodlee/YodleeWebViewFragment.kt
+++ app/src/main/java/piuk/blockchain/android/ui/linkbank/yodlee/YodleeWebViewFragment.kt
@@ -64,7 +64,7 @@ class YodleeWebViewFragment :
     private val bearerParam: String by lazy { "Bearer ${attributes.token}" }
     private val extraParamsKey = "extraParams"
     private val extraParamConfigName: String
-        get() = "configName=${attributes.configName}&intentUrl=https://${BuildConfig.YODLEE_DEEPLINK_HOST}"
+        get() = "configName=${attributes.configName}&intentUrl=${BuildConfig.BROKERAGE_SUCCESS}"
     private val extraParamEncoding = "UTF-8"
 
     private val yodleeQuery: String by lazy {
