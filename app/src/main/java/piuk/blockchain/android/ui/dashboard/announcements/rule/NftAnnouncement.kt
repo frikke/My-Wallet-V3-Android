402fa99f7c1d99d297fd218298563a3c2797c910
Othman El Jazouli <oteljazouli@blockchain.com>
2022-06-03 13:07:05 +0200

    [AND-6245 - Announcement for NFT] setup announcement

---
+++ app/src/main/java/piuk/blockchain/android/ui/dashboard/announcements/rule/NftAnnouncement.kt
@@ -0,0 +1,47 @@
+package piuk.blockchain.android.ui.dashboard.announcements.rule
+
+import androidx.annotation.VisibleForTesting
+import io.reactivex.rxjava3.core.Single
+import piuk.blockchain.android.R
+import piuk.blockchain.android.ui.dashboard.announcements.AnnouncementHost
+import piuk.blockchain.android.ui.dashboard.announcements.AnnouncementRule
+import piuk.blockchain.android.ui.dashboard.announcements.DismissRecorder
+import piuk.blockchain.android.ui.dashboard.announcements.DismissRule
+import piuk.blockchain.android.ui.dashboard.announcements.StandardAnnouncementCard
+
+class NftAnnouncement(
+    dismissRecorder: DismissRecorder
+) : AnnouncementRule(dismissRecorder) {
+
+    override val dismissKey = DISMISS_KEY
+
+    override fun shouldShow(): Single<Boolean> = Single.just(!dismissEntry.isDismissed)
+
+    override fun show(host: AnnouncementHost) {
+        host.showAnnouncementCard(
+            card = StandardAnnouncementCard(
+                name = name,
+                dismissRule = DismissRule.CardPersistent,
+                dismissEntry = dismissEntry,
+                titleText = R.string.nft_announcement_title,
+                bodyText = R.string.nft_announcement_description,
+                ctaText = R.string.nft_announcement_action,
+                iconImage = R.drawable.ic_nft,
+                shouldWrapIconWidth = false,
+                dismissFunction = {
+                    host.dismissAnnouncementCard()
+                },
+                ctaFunction = {
+                    host.dismissAnnouncementCard()
+                }
+            )
+        )
+    }
+
+    override val name = "view_nft_waitlist"
+
+    companion object {
+        @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+        const val DISMISS_KEY = "NftAnnouncement_DISMISSED"
+    }
+}
