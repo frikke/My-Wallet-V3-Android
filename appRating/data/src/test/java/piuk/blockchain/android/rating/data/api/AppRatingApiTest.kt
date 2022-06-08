e14a8457217d511532f0b0b27ccdb8c9b4817452
Othman El Jazouli <oteljazouli@blockchain.com>
2022-06-08 18:28:47 +0200

    [AND-6263 app rating] fix unittest

--- appRating/data/src/test/java/piuk/blockchain/android/rating/data/api/AppRatingApiTest.kt
+++ appRating/data/src/test/java/piuk/blockchain/android/rating/data/api/AppRatingApiTest.kt
@@ -3,12 +3,14 @@ package piuk.blockchain.android.rating.data.api
 import com.blockchain.outcome.Outcome
 import io.mockk.coEvery
 import io.mockk.coVerify
+import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
 import piuk.blockchain.android.rating.data.model.AppRatingApiKeys
 import piuk.blockchain.android.rating.data.model.RespondentResponse
+import piuk.blockchain.android.rating.data.model.RespondentResponseData
 import piuk.blockchain.android.rating.data.model.SurveyBody
 import piuk.blockchain.android.rating.domain.model.AppRating
 
@@ -29,6 +31,7 @@ class AppRatingApiTest {
     fun `WHEN postRatingData is called, THEN arguments should be correctly spread to the endpoint`() = runTest {
         coEvery { appRatingEndpoints.postRatingData(any(), any(), any(), any()) } returns
             Outcome.Success(respondentResponse)
+        every { respondentResponse.data } returns RespondentResponseData(succeeded = true)
 
         appRatingApi.postRatingData(apiKeys, appRating)
 
