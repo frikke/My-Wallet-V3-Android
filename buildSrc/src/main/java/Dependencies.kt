a684e7886c4ec1ee59d434f5ffae8ba63c48f311
Othman El Jazouli <oteljazouli@blockchain.com>
2022-06-30 13:40:08 +0200

    version name 7.2.0

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -8,7 +8,7 @@ object Versions {
     const val compileSdk = 31
 
     const val versionCode = 20422
-    const val versionName = "202207.1.0"
+    const val versionName = "202207.2.0"
 
     const val buildTools = "30.0.2"
 
