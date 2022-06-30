d7dbe4884ca12f182160fe17ed7876d48120e24d
Othman El Jazouli <oteljazouli@blockchain.com>
2022-06-30 16:30:14 +0200

    [SUX-25-defi-onboarding] fix activity result nav

--- app/src/main/java/piuk/blockchain/android/ui/dashboard/walletmode/WalletModeSelectionBottomSheet.kt
+++ app/src/main/java/piuk/blockchain/android/ui/dashboard/walletmode/WalletModeSelectionBottomSheet.kt
@@ -15,6 +15,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.dimensionResource
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.blockchain.commonarch.presentation.mvi_v2.MVIBottomSheet
 import com.blockchain.commonarch.presentation.mvi_v2.ModelConfigArgs
 import com.blockchain.commonarch.presentation.mvi_v2.NavigationRouter
@@ -24,6 +27,9 @@ import com.blockchain.extensions.exhaustive
 import com.blockchain.koin.payloadScope
 import com.blockchain.presentation.onboarding.DeFiOnboardingActivity
 import com.blockchain.walletmode.WalletMode
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import org.koin.android.scope.AndroidScopeComponent
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.scope.Scope
@@ -53,7 +59,24 @@ class WalletModeSelectionBottomSheet :
         ActivityResultContracts.StartActivityForResult()
     ) {
         if (it.resultCode == Activity.RESULT_OK) {
-            deFiOnboardingComplete()
+            /**
+             * IMPORTANT
+             *
+             * mandatory Dispatchers.IO otherwise the nav event is not caught as we're coming back from another activity
+             * has to do with running things serially in main thread where [Lifecycle.repeatOnLifecycle]
+             * is supposed to start collecting
+             *
+             * With the navigation flow being lifecycle aware and tied to [Lifecycle.State.STARTED],
+             * [Lifecycle.repeatOnLifecycle] with [Lifecycle.State.RESUMED] ↓ should've been enough
+             * so not sure if it's a lifecycle edge case
+             */
+            var job: Job? = null
+            job = lifecycleScope.launch(Dispatchers.IO) {
+                lifecycle.repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                    deFiOnboardingComplete()
+                    job?.cancel()
+                }
+            }
         }
     }
 
