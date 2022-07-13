c0026b876da53b93fec3a2dff6ad8837f3c4cdb5
Othman El Jazouli <oteljazouli@blockchain.com>
2022-07-13 14:54:29 +0200

    [AND-000-fix-navigation]

--- app/src/main/java/piuk/blockchain/android/walletmode/WalletModeThemeProvider.kt
+++ app/src/main/java/piuk/blockchain/android/walletmode/WalletModeThemeProvider.kt
@@ -1,17 +1,9 @@
 package piuk.blockchain.android.walletmode
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ReadOnlyComposable
-import com.blockchain.componentlib.theme.AppDimensions
-import com.blockchain.componentlib.theme.AppShapes
 import com.blockchain.componentlib.theme.AppThemeProvider
-import com.blockchain.componentlib.theme.AppTypography
 import com.blockchain.componentlib.theme.DefaultAppTheme
-import com.blockchain.componentlib.theme.Purple0000
-import com.blockchain.componentlib.theme.SemanticColors
+import com.blockchain.componentlib.theme.DefiWalletTheme
 import com.blockchain.componentlib.theme.Theme
-import com.blockchain.componentlib.theme.defDarkColors
-import com.blockchain.componentlib.theme.defLightColors
 import com.blockchain.walletmode.WalletMode
 import com.blockchain.walletmode.WalletModeService
 import kotlinx.coroutines.flow.Flow
@@ -27,31 +19,4 @@ class WalletModeThemeProvider(private val walletModeService: WalletModeService) 
                 WalletMode.NON_CUSTODIAL_ONLY -> DefiWalletTheme
             }
         }
-}
-
-object DefiWalletTheme : Theme() {
-    override val lightColors: SemanticColors
-        @Composable
-        @ReadOnlyComposable
-        get() = defLightColors.copy(primary = Purple0000)
-
-    override val darkColors: SemanticColors
-        @Composable
-        @ReadOnlyComposable
-        get() = defDarkColors
-
-    override val typography: AppTypography
-        @Composable
-        @ReadOnlyComposable
-        get() = AppTypography()
-
-    override val dimensions: AppDimensions
-        @Composable
-        @ReadOnlyComposable
-        get() = AppDimensions()
-
-    override val shapes: AppShapes
-        @Composable
-        @ReadOnlyComposable
-        get() = AppShapes()
 }
