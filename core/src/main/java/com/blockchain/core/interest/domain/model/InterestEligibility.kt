0ec8a6e9d3aaae16ddf08da357f7e67b4fda5ed9
Othman El Jazouli <oteljazouli@blockchain.com>
2022-07-28 17:53:37 +0200

    [AND-000-refactor-interest-operations] ref

--- core/src/main/java/com/blockchain/core/interest/domain/model/InterestEligibility.kt
+++ core/src/main/java/com/blockchain/core/interest/domain/model/InterestEligibility.kt
@@ -1,10 +1,5 @@
 package com.blockchain.core.interest.domain.model
 
-// data class AssetInterestEligibility(
-//    val cryptoCurrency: AssetInfo,
-//    val eligibility: Eligibility
-// )
-
 sealed interface InterestEligibility {
     object Eligible : InterestEligibility
 
@@ -19,19 +14,3 @@ sealed interface InterestEligibility {
         }
     }
 }
-
-// data class Eligibility(
-//    val eligible: Boolean,
-//    val ineligibilityReason: IneligibilityReason
-// ) {
-//    companion object {
-//        fun notEligibleInstance() = Eligibility(false, IneligibilityReason.OTHER)
-//    }
-// }
-//
-// enum class IneligibilityReason {
-//    REGION,
-//    KYC_TIER,
-//    OTHER,
-//    NONE
-// }
