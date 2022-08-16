0d351b8dcdf1e87ea0ba8f6df6d1d7ff6c96e921
Othman El Jazouli <oteljazouli@blockchain.com>
2022-08-16 19:05:52 +0200

    [AND-000-referral-screen-changes] ktlint

--- app/src/main/java/piuk/blockchain/android/ui/referral/presentation/composable/ReferralScreen.kt
+++ app/src/main/java/piuk/blockchain/android/ui/referral/presentation/composable/ReferralScreen.kt
@@ -23,7 +23,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.annotation.ExperimentalCoilApi
