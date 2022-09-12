b57d414ed3e3220f3b080ca56987c8bdcf67f053
antonis-bc <49150635+antonis-bc@users.noreply.github.com>
2022-09-12 15:35:59 +0300

    fix(walletmode): AND-0000 Reset prices filter on switch (#3878)

--- app/src/main/java/piuk/blockchain/android/ui/prices/presentation/PricesViewModel.kt
+++ app/src/main/java/piuk/blockchain/android/ui/prices/presentation/PricesViewModel.kt
@@ -112,6 +112,7 @@ class PricesViewModel(
                 updateState {
                     modelState.copy(
                         isLoadingData = true,
+                        filterBy = "",
                         data = emptyList()
                     )
                 }
