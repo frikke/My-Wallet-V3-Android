14a140e420f88472dae7fffec26642d55103ed86
Othman El Jazouli <oteljazouli@blockchain.com>
2022-10-10 20:19:46 +0100

    [AND-6335-nft-crop collection]

--- nfts/data/src/main/java/com/blockchain/nfts/data/repository/NftRepository.kt
+++ nfts/data/src/main/java/com/blockchain/nfts/data/repository/NftRepository.kt
@@ -45,38 +45,43 @@ class NftRepository(private val nftCollectionStore: NftCollectionStore) : NftSer
         }
     }
 
-    private fun NftAssetsDto.mapToDomain(pageKey: String?): NftAssetsPage = run {
-        NftAssetsPage(
-            assets = this.assets.filterNot { it.imageUrl.isNullOrBlank() }
-                .map { nftAsset ->
-                    NftAsset(
-                        id = nftAsset.id.orEmpty(),
-                        pageKey = pageKey,
-                        tokenId = nftAsset.tokenId.orEmpty(),
-                        imageUrl = nftAsset.imageUrl ?: nftAsset.imagePreviewUrl.orEmpty(),
-                        name = nftAsset.name.orEmpty(),
-                        description = nftAsset.description.orEmpty(),
-                        contract = NftContract(nftAsset.contract.address),
-                        creator = NftCreator(
-                            imageUrl = nftAsset.creator.imageUrl,
-                            name = nftAsset.creator.address.let {
-                                if (it.lowercase().startsWith("0x")) {
-                                    it.drop(2).substring(0..5)
-                                } else {
-                                    it
-                                }
-                            },
-                            isVerified = nftAsset.creator.isVerified
-                        ),
-                        traits = nftAsset.traits.map { nftTrait ->
-                            NftTrait(
-                                name = nftTrait.name,
-                                value = nftTrait.value
-                            )
-                        }
-                    )
-                },
-            nextPageKey = this.next
-        )
+    companion object {
+        private const val PREFIX_0X = "0x"
+        private val EXTRACT_NAME_RAGE = 0..5
+
+        private fun NftAssetsDto.mapToDomain(pageKey: String?): NftAssetsPage = run {
+            NftAssetsPage(
+                assets = this.assets.filterNot { it.imageUrl.isNullOrBlank() }
+                    .map { nftAsset ->
+                        NftAsset(
+                            id = nftAsset.id.orEmpty(),
+                            pageKey = pageKey,
+                            tokenId = nftAsset.tokenId.orEmpty(),
+                            imageUrl = nftAsset.imageUrl ?: nftAsset.imagePreviewUrl.orEmpty(),
+                            name = nftAsset.name.orEmpty(),
+                            description = nftAsset.description.orEmpty(),
+                            contract = NftContract(nftAsset.contract.address),
+                            creator = NftCreator(
+                                imageUrl = nftAsset.creator.imageUrl,
+                                name = nftAsset.creator.address.let {
+                                    if (it.lowercase().startsWith(PREFIX_0X)) {
+                                        it.drop(PREFIX_0X.length).substring(EXTRACT_NAME_RAGE)
+                                    } else {
+                                        it
+                                    }
+                                },
+                                isVerified = nftAsset.creator.isVerified
+                            ),
+                            traits = nftAsset.traits.map { nftTrait ->
+                                NftTrait(
+                                    name = nftTrait.name,
+                                    value = nftTrait.value
+                                )
+                            }
+                        )
+                    },
+                nextPageKey = this.next
+            )
+        }
     }
 }
