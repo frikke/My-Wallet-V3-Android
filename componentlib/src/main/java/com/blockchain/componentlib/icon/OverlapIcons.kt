9b60792ea221cfd689513a3bd1ae2a06d6b80cdb
Othman El Jazouli <oteljazouli@blockchain.com>
2022-11-02 16:44:34 +0100

    [AND-6728 - stacked icon / navbar]

---
+++ componentlib/src/main/java/com/blockchain/componentlib/icon/OverlapIcons.kt
@@ -0,0 +1,65 @@
+package com.blockchain.componentlib.icon
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.blockchain.componentlib.R
+import com.blockchain.componentlib.basic.Image
+import com.blockchain.componentlib.basic.ImageResource
+import com.blockchain.componentlib.theme.AppTheme
+
+@Composable
+fun OverlapIcons(
+    leadingImagePrimary: ImageResource,
+    leadingImageSecondary: ImageResource,
+    borderColor: Color = AppTheme.colors.background
+) {
+    val borderSize = 2.dp
+    val iconSize = 18.dp
+    val overlap = 6.dp
+
+    Box(
+        modifier = Modifier
+            .size(
+                height = iconSize * 2 - overlap + borderSize,
+                width = iconSize + overlap + borderSize
+            )
+    ) {
+        Image(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .size(iconSize)
+                .background(color = borderColor, shape = CircleShape)
+                .border(width = AppTheme.dimensions.noSpacing, Color.Transparent, shape = CircleShape),
+            imageResource = leadingImageSecondary
+        )
+
+        Image(
+            modifier = Modifier
+                .size(iconSize + borderSize * 2)
+                .background(color = borderColor, shape = CircleShape)
+                .border(width =borderSize, borderColor, CircleShape)
+                .padding(borderSize),
+            imageResource = leadingImagePrimary
+        )
+    }
+}
+
+@Preview(backgroundColor = 0xFFFFFFFF, showBackground = true)
+@Composable
+fun PreviewOverlapIcons() {
+    OverlapIcons(
+        leadingImagePrimary = ImageResource.Local(R.drawable.ic_close_circle_dark),
+        leadingImageSecondary = ImageResource.Local(R.drawable.ic_close_circle),
+        borderColor = AppTheme.colors.background
+    )
+}
---
+++ componentlib/src/main/java/com/blockchain/componentlib/icon/SmallTagIcons.kt
@@ -0,0 +1,64 @@
+package com.blockchain.componentlib.icon
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.blockchain.componentlib.R
+import com.blockchain.componentlib.basic.Image
+import com.blockchain.componentlib.basic.ImageResource
+import com.blockchain.componentlib.theme.AppTheme
+
+@Composable
+fun SmallTagIcons(
+    leadingImagePrimary: ImageResource,
+    leadingImageSecondary: ImageResource,
+    borderColor: Color = AppTheme.colors.background
+) {
+    val borderSize = AppTheme.dimensions.composeSmallestSpacing
+    val mainIconSize = AppTheme.dimensions.standardSpacing
+    val tagIconSize = AppTheme.dimensions.verySmallSpacing
+    val overlap = 10.dp
+
+    Box(
+        modifier = Modifier
+            .size(mainIconSize + tagIconSize - overlap + borderSize)
+    ) {
+
+        Image(
+            modifier = Modifier
+                .size(mainIconSize)
+                .background(color = borderColor, shape = CircleShape)
+                .border(width = AppTheme.dimensions.noSpacing, Color.Transparent, shape = CircleShape),
+            imageResource = leadingImagePrimary
+        )
+
+        Image(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .size(tagIconSize + borderSize * 2)
+                .background(color = borderColor, shape = CircleShape)
+                .border(width = borderSize, borderColor, CircleShape)
+                .padding(borderSize),
+            imageResource = leadingImageSecondary
+        )
+    }
+}
+
+@Preview(backgroundColor = 0xFFFFFFFF, showBackground = true)
+@Composable
+fun PreviewSmallTagIcons() {
+    SmallTagIcons(
+        leadingImagePrimary = ImageResource.Local(R.drawable.ic_close_circle_dark),
+        leadingImageSecondary = ImageResource.Local(R.drawable.ic_close_circle),
+        borderColor = AppTheme.colors.background
+    )
+}
