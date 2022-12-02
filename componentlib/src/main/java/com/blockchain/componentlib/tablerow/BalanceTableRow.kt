558ba615ab9bc8ee1eb4ccc844dd8fef180f5bec
dserrano-bc <64006245+dserrano-bc@users.noreply.github.com>
2022-12-02 16:58:57 +0100

    feat(staking): AND-6802 summary sheet extra info (#4181)

--- componentlib/src/main/java/com/blockchain/componentlib/tablerow/BalanceTableRow.kt
+++ componentlib/src/main/java/com/blockchain/componentlib/tablerow/BalanceTableRow.kt
@@ -1,5 +1,6 @@
 package com.blockchain.componentlib.tablerow
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.requiredSizeIn
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -16,6 +18,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.blockchain.componentlib.R
@@ -35,6 +39,8 @@ fun BalanceTableRow(
     bodyEnd: AnnotatedString? = null,
     startImageResource: ImageResource = ImageResource.None,
     endImageResource: ImageResource = ImageResource.None,
+    postStartTitleImageResource: ImageResource = ImageResource.None,
+    postStartTitleImageResourceOnClick: () -> Unit = {},
     isInlineTags: Boolean = false,
     tags: List<TagViewState> = emptyList(),
     onClick: () -> Unit = {}
@@ -52,13 +58,13 @@ fun BalanceTableRow(
         },
         content = {
             val startPadding = if (startImageResource != ImageResource.None) {
-                dimensionResource(R.dimen.medium_spacing)
+                dimensionResource(R.dimen.small_spacing)
             } else {
                 dimensionResource(R.dimen.zero_spacing)
             }
 
             val endPadding = if (endImageResource != ImageResource.None) {
-                dimensionResource(R.dimen.medium_spacing)
+                dimensionResource(R.dimen.small_spacing)
             } else {
                 dimensionResource(R.dimen.zero_spacing)
             }
@@ -68,12 +74,50 @@ fun BalanceTableRow(
                     .fillMaxWidth()
                     .wrapContentHeight()
             ) {
-                TableRowText(
-                    startText = titleStart,
-                    endText = titleEnd,
-                    textStyle = AppTheme.typography.body2,
-                    textColor = AppTheme.colors.title
-                )
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight()
+                ) {
+                    Text(
+                        text = titleStart,
+                        style = AppTheme.typography.body2,
+                        modifier = if (postStartTitleImageResource == ImageResource.None) {
+                            Modifier.weight(1f)
+                        } else {
+                            Modifier.wrapContentWidth()
+                        },
+                        textAlign = TextAlign.Start,
+                        color = AppTheme.colors.title,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+
+                    if (postStartTitleImageResource != ImageResource.None) {
+                        Image(
+                            imageResource = postStartTitleImageResource,
+                            modifier = Modifier
+                                .padding(AppTheme.dimensions.smallestSpacing)
+                                .clickable(onClick = postStartTitleImageResourceOnClick)
+                                .size(AppTheme.dimensions.smallSpacing)
+                        )
+                    }
+
+                    if (titleEnd != null) {
+                        Text(
+                            text = titleEnd,
+                            style = AppTheme.typography.body2,
+                            modifier = if (postStartTitleImageResource == ImageResource.None) {
+                                Modifier.wrapContentSize()
+                            } else {
+                                Modifier.weight(1f)
+                            },
+                            textAlign = TextAlign.End,
+                            color = AppTheme.colors.title
+                        )
+                    }
+                }
 
                 Row(
                     modifier = Modifier
@@ -118,15 +162,22 @@ fun BalanceTableRow(
                     modifier = Modifier.requiredSizeIn(
                         maxWidth = dimensionResource(R.dimen.standard_spacing),
                         maxHeight = dimensionResource(R.dimen.standard_spacing),
-                    ),
+                    )
                 )
             }
         },
         contentBottom = {
-            if (!isInlineTags && !tags.isNullOrEmpty()) {
+            if (!isInlineTags && tags.isNotEmpty()) {
                 TagsRow(
                     tags = tags,
-                    modifier = Modifier.padding(top = 8.dp, start = 40.dp)
+                    modifier = Modifier.padding(
+                        top = if (bodyStart != null) {
+                            AppTheme.dimensions.tinySpacing
+                        } else {
+                            0.dp
+                        },
+                        start = AppTheme.dimensions.hugeSpacing
+                    )
                 )
             }
         },
@@ -144,6 +195,24 @@ fun BalanceTableRow_Local_ImageStart() {
                 bodyStart = buildAnnotatedString { append("Some body here") },
                 onClick = {},
                 startImageResource = ImageResource.Local(
+                    id = R.drawable.ic_blockchain,
+                ),
+                tags = emptyList()
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun BalanceTableRow_Local_ImageEnd() {
+    AppTheme {
+        AppSurface {
+            BalanceTableRow(
+                titleStart = buildAnnotatedString { append("Some title here") },
+                bodyStart = buildAnnotatedString { append("Some body here") },
+                onClick = {},
+                endImageResource = ImageResource.Local(
                     id = R.drawable.ic_blockchain,
                 ),
                 tags = emptyList()
@@ -221,6 +290,47 @@ fun BalanceTableRow_TitleStart_Tags_Inline() {
                 tags = listOf(TagViewState("One", TagType.Default()), TagViewState("Two", TagType.Success())),
                 isInlineTags = true,
                 startImageResource = ImageResource.Local(
+                    id = R.drawable.ic_blockchain,
+                ),
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun BalanceTableRow_TitleStart_Tags_NoBodyStart() {
+    AppTheme {
+        AppSurface {
+            BalanceTableRow(
+                titleStart = buildAnnotatedString { append("Some title here") },
+                titleEnd = buildAnnotatedString { append("100 BTC") },
+                bodyEnd = buildAnnotatedString { append("$100") },
+                onClick = {},
+                tags = listOf(TagViewState("One", TagType.Default()), TagViewState("Two", TagType.Success())),
+                startImageResource = ImageResource.Local(
+                    id = R.drawable.ic_blockchain,
+                ),
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun BalanceTableRow_PostTitleImageResource() {
+    AppTheme {
+        AppSurface {
+            BalanceTableRow(
+                titleStart = buildAnnotatedString { append("Some title here") },
+                titleEnd = buildAnnotatedString { append("100 BTC") },
+                bodyEnd = buildAnnotatedString { append("$100") },
+                onClick = {},
+                tags = listOf(TagViewState("One", TagType.Default()), TagViewState("Two", TagType.Success())),
+                startImageResource = ImageResource.Local(
+                    id = R.drawable.ic_blockchain,
+                ),
+                postStartTitleImageResource = ImageResource.Local(
                     id = R.drawable.ic_blockchain,
                 ),
             )
