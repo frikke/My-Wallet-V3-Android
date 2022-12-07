1082e3a0b0ef368f265871ae2ca76dde2ddb4c54
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2022-12-07 13:40:16 +0000

    chore(release): AND-0000 Version Code Bump: 24773

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -8,7 +8,7 @@ object Versions {
     const val targetSdk = 33
     const val compileSdk = 33
 
-    const val versionCode = 24767
+    const val versionCode = 24773
     const val versionName = "202212.1.0"
 
     const val buildTools = "30.0.3"
