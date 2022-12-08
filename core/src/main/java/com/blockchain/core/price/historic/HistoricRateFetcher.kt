a8fba2d296c9cabc6135d5d9ea8fba0863d9e238
Othman El Jazouli <oteljazouli@blockchain.com>
2022-12-08 12:21:42 +0100

    [AND-6684-Visualise-activity-detail-custodial 7] ktlint

--- core/src/main/java/com/blockchain/core/price/historic/HistoricRateFetcher.kt
+++ core/src/main/java/com/blockchain/core/price/historic/HistoricRateFetcher.kt
@@ -2,15 +2,11 @@ package com.blockchain.core.price.historic
 
 import com.blockchain.data.DataResource
 import com.blockchain.data.KeyedFreshnessStrategy
-import com.blockchain.outcome.map
-import com.blockchain.store.firstOutcome
 import com.blockchain.store.mapData
-import com.blockchain.utils.rxSingleOutcome
 import info.blockchain.balance.AssetInfo
 import info.blockchain.balance.ExchangeRate
 import info.blockchain.balance.FiatCurrency
 import info.blockchain.balance.Money
-import io.reactivex.rxjava3.core.Single
 import kotlinx.coroutines.flow.Flow
 
 // Moved here fromm app. This should be under, rather than over, ExchangeRateDM TODO
