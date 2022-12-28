3b74004c30c7cce77794c49925653d3ca491d202
Othman El Jazouli <oteljazouli@blockchain.com>
2022-12-28 15:29:26 +0100

    fix navbar crash - kt

--- commonarch/src/main/java/com/blockchain/commonarch/presentation/base/BlockchainActivity.kt
+++ commonarch/src/main/java/com/blockchain/commonarch/presentation/base/BlockchainActivity.kt
@@ -30,7 +30,7 @@ import com.blockchain.componentlib.databinding.ToolbarGeneralBinding
 import com.blockchain.componentlib.legacy.MaterialProgressDialog
 import com.blockchain.componentlib.navigation.NavigationBarButton
 import com.blockchain.enviroment.EnvironmentConfig
-import com.blockchain.instrumentation.InstrumentationScaffold
+import com.blockchain.instrumentation.InstrumentationScaffold // ktlint-disable instrumentation-ruleset:no-instrumentation-import
 import com.blockchain.koin.payloadScope
 import com.blockchain.koin.superAppModeService
 import com.blockchain.logging.RemoteLogger
