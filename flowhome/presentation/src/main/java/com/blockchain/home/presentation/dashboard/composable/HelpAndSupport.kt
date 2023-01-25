a2ba12084abd9de2edf3bbf4bd57adaa81c36e23
labreu-bc <97444041+labreu-bc@users.noreply.github.com>
2023-01-25 17:05:03 +0000

    Replace text with simpletext (#4343)

--- flowhome/presentation/src/main/java/com/blockchain/home/presentation/dashboard/composable/HelpAndSupport.kt
+++ flowhome/presentation/src/main/java/com/blockchain/home/presentation/dashboard/composable/HelpAndSupport.kt
@@ -12,9 +12,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.blockchain.componentlib.basic.ComposeColors
+import com.blockchain.componentlib.basic.ComposeGravities
+import com.blockchain.componentlib.basic.ComposeTypographies
+import com.blockchain.componentlib.basic.SimpleText
 import com.blockchain.componentlib.tablerow.DefaultTableRow
 import com.blockchain.componentlib.theme.AppTheme
-import com.blockchain.componentlib.theme.Grey700
 import com.blockchain.home.presentation.R
 
 @Composable
@@ -26,12 +29,16 @@ fun HelpAndSupport(
             .fillMaxWidth()
             .padding(AppTheme.dimensions.smallSpacing)
     ) {
-        Text(
+
+        SimpleText(
             text = stringResource(R.string.ma_home_need_help),
-            style = AppTheme.typography.body2,
-            color = Grey700
+            style = ComposeTypographies.Body2,
+            color = ComposeColors.Body,
+            gravity = ComposeGravities.Start
         )
+
         Spacer(modifier = Modifier.size(AppTheme.dimensions.smallSpacing))
+
         Card(
             backgroundColor = AppTheme.colors.background,
             shape = RoundedCornerShape(AppTheme.dimensions.mediumSpacing),
