34e3defd08f62b9a73bf63d1103fbaefeb7a2dc6
Othman El Jazouli <oteljazouli@blockchain.com>
2023-02-14 20:05:59 +0100

    [GROWUX-78 Top 4 movers dash]

--- flowhome/presentation/src/main/java/com/blockchain/home/presentation/dashboard/composable/HomeActivity.kt
+++ flowhome/presentation/src/main/java/com/blockchain/home/presentation/dashboard/composable/HomeActivity.kt
@@ -1,14 +1,9 @@
 package com.blockchain.home.presentation.dashboard.composable
 
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyListScope
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -16,9 +11,8 @@ import com.blockchain.analytics.Analytics
 import com.blockchain.componentlib.R
 import com.blockchain.componentlib.lazylist.paddedItem
 import com.blockchain.componentlib.lazylist.paddedRoundedCornersItems
+import com.blockchain.componentlib.tablerow.TableRowHeader
 import com.blockchain.componentlib.theme.AppTheme
-import com.blockchain.componentlib.theme.Grey700
-import com.blockchain.componentlib.utils.clickableNoEffect
 import com.blockchain.data.DataResource
 import com.blockchain.home.presentation.activity.common.ActivityComponentItem
 import com.blockchain.home.presentation.activity.common.ClickAction
@@ -27,30 +21,6 @@ import com.blockchain.home.presentation.activity.list.TransactionGroup
 import com.blockchain.home.presentation.dashboard.DashboardAnalyticsEvents
 import com.blockchain.walletmode.WalletMode
 import org.koin.androidx.compose.get
-
-@Composable
-fun HomeActivityHeader(
-    analytics: Analytics = get(),
-    openActivity: () -> Unit
-) {
-    Row(modifier = Modifier.fillMaxWidth()) {
-        Text(
-            text = stringResource(R.string.ma_home_activity_title),
-            style = AppTheme.typography.body2,
-            color = Grey700
-        )
-        Spacer(modifier = Modifier.weight(1f))
-        Text(
-            modifier = Modifier.clickableNoEffect {
-                openActivity()
-                analytics.logEvent(DashboardAnalyticsEvents.ActivitySeeAllClicked)
-            },
-            text = stringResource(R.string.see_all),
-            style = AppTheme.typography.paragraph2,
-            color = AppTheme.colors.primary,
-        )
-    }
-}
 
 fun LazyListScope.homeActivityScreen(
     activityState: ActivityViewState,
@@ -64,8 +34,16 @@ fun LazyListScope.homeActivityScreen(
         paddedItem(
             paddingValues = PaddingValues(horizontal = 16.dp)
         ) {
+            val analytics: Analytics = get()
             Spacer(modifier = Modifier.size(AppTheme.dimensions.largeSpacing))
-            HomeActivityHeader(openActivity = openActivity)
+            TableRowHeader(
+                title = stringResource(R.string.ma_home_activity_title),
+                actionTitle = stringResource(R.string.see_all),
+                actionOnClick = {
+                    openActivity()
+                    analytics.logEvent(DashboardAnalyticsEvents.ActivitySeeAllClicked)
+                }
+            )
             Spacer(modifier = Modifier.size(AppTheme.dimensions.tinySpacing))
         }
         paddedRoundedCornersItems(
