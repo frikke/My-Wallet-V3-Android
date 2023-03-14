70af48d6e29f547291cd3dee79b11782cdcfb434
Othman El Jazouli <oteljazouli@blockchain.com>
2023-03-14 11:55:34 +0100

    [GROWUX-291 top movers FF in buy] buy_top_movers_enabled to user trait

--- app/src/main/java/piuk/blockchain/android/walletmode/WalletModeTraitsRepository.kt
+++ app/src/main/java/piuk/blockchain/android/walletmode/WalletModeTraitsRepository.kt
@@ -1,7 +1,9 @@
 package piuk.blockchain.android.walletmode
 
 import com.blockchain.analytics.TraitsService
+import com.blockchain.featureflag.FeatureFlag
 import com.blockchain.koin.payloadScope
+import com.blockchain.koin.topMoversInBuy
 import com.blockchain.walletmode.WalletMode
 import com.blockchain.walletmode.WalletModeService
 import kotlinx.coroutines.rx3.await
@@ -9,11 +11,13 @@ import kotlinx.coroutines.rx3.await
 class WalletModeTraitsRepository : TraitsService {
     override suspend fun traits(): Map<String, String> {
         val walletModeService = payloadScope.getOrNull<WalletModeService>()
+        val topMoversInBuyFF = payloadScope.getOrNull<FeatureFlag>(topMoversInBuy)
         return if (walletModeService != null) {
             val walletMode = walletModeService.walletModeSingle.await()
             mapOf(
                 "is_superapp_v1" to true.toString(),
                 "app_mode" to walletMode.toTraitsString(),
+                "buy_top_movers_enabled" to (topMoversInBuyFF?.coEnabled() ?: false).toString()
             )
         } else
             emptyMap()
