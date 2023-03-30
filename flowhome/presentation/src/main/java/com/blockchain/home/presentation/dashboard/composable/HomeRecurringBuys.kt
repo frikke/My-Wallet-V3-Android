4426df88f55655c55611bc8a980f660e8579c532
Othman El Jazouli <oteljazouli@blockchain.com>
2023-03-30 13:52:00 +0000

    [GROWUX-317-rb-in-home] upsell

--- flowhome/presentation/src/main/java/com/blockchain/home/presentation/dashboard/composable/HomeRecurringBuys.kt
+++ flowhome/presentation/src/main/java/com/blockchain/home/presentation/dashboard/composable/HomeRecurringBuys.kt
@@ -2,15 +2,23 @@ package com.blockchain.home.presentation.dashboard.composable
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.blockchain.analytics.events.LaunchOrigin
 import com.blockchain.componentlib.R
+import com.blockchain.componentlib.icons.Icons
+import com.blockchain.componentlib.icons.Sync
 import com.blockchain.componentlib.lazylist.paddedItem
 import com.blockchain.componentlib.lazylist.paddedRoundedCornersItems
+import com.blockchain.componentlib.tablerow.ButtonTableRow
 import com.blockchain.componentlib.tablerow.TableRowHeader
 import com.blockchain.componentlib.theme.AppTheme
 import com.blockchain.componentlib.utils.value
@@ -34,7 +42,22 @@ internal fun LazyListScope.homeRecurringBuys(
     }
 
     if (recurringBuys.isEmpty()) {
-        // upsell
+        paddedItem(
+            paddingValues = PaddingValues(horizontal = 16.dp)
+        ) {
+            Surface(
+                shape = RoundedCornerShape(AppTheme.dimensions.borderRadiiMedium)
+            ) {
+                ButtonTableRow(
+                    title = stringResource(R.string.recurring_buy_automate_title),
+                    subtitle = stringResource(R.string.recurring_buy_automate_description),
+                    imageResource = Icons.Filled.Sync.withTint(AppTheme.colors.primary),
+                    actionText = stringResource(R.string.common_go),
+                    onClick = {
+                    }
+                )
+            }
+        }
     } else {
         paddedRoundedCornersItems(
             items = recurringBuys,
