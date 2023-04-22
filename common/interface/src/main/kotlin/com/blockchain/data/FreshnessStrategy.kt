6e65b74c03f0da2daae1c0037eeff6b3276f095d
Andre Romano <aromano@blockchain.com>
2023-04-22 02:30:01 +0100

    cleanup

--- common/interface/src/main/kotlin/com/blockchain/data/FreshnessStrategy.kt
+++ common/interface/src/main/kotlin/com/blockchain/data/FreshnessStrategy.kt
@@ -1,6 +1,5 @@
 package com.blockchain.data
 
-import java.io.Serializable
 import java.util.concurrent.TimeUnit
 
 /**
@@ -12,7 +11,7 @@ import java.util.concurrent.TimeUnit
  *      - should fetch == false: `[Data(cache), Data(future cache change)]`
  *   - [Cached(refreshStrategy=RefreshStrategy.RefreshIfOlderThan(5, TimeUnit.MINUTES))] will get the latest cache, and only fetch if there is no cached data, the mediator decides to fetch or if the cached data is older than the passed time, it will also listen for future cache changes
  */
-sealed class FreshnessStrategy : Serializable {
+sealed class FreshnessStrategy {
     object Fresh : FreshnessStrategy()
     data class Cached(val refreshStrategy: RefreshStrategy) : FreshnessStrategy()
 
@@ -26,7 +25,7 @@ sealed class FreshnessStrategy : Serializable {
     }
 }
 
-sealed class RefreshStrategy : Serializable {
+sealed class RefreshStrategy {
     object ForceRefresh : RefreshStrategy()
     object RefreshIfStale : RefreshStrategy()
     data class RefreshIfOlderThan(
@@ -41,7 +40,7 @@ sealed class RefreshStrategy : Serializable {
  * Keyed version of [FreshnessStrategy]
  * See [FreshnessStrategy]  for more detailed documentation.
  */
-sealed class KeyedFreshnessStrategy<out K> : Serializable {
+sealed class KeyedFreshnessStrategy<out K> {
     data class Fresh<out K>(val key: K) : KeyedFreshnessStrategy<K>()
     data class Cached<out K>(val key: K, val refreshStrategy: RefreshStrategy) : KeyedFreshnessStrategy<K>()
 }
