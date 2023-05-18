3fbc7ba89249f45d268d0a6d2040085403f62938
Othman El Jazouli <oteljazouli@blockchain.com>
2023-05-18 13:16:11 +0200

    [GROWUX-296 news presentation] kt

--- blockchainApi/src/main/java/com/blockchain/api/news/NewsArticlesDto.kt
+++ blockchainApi/src/main/java/com/blockchain/api/news/NewsArticlesDto.kt
@@ -11,13 +11,13 @@ data class NewsArticlesDto(
 data class Article(
     val id: Int,
     val title: String,
-    val image: String?,
+    val image: String? = null,
     val date: String,
-    val author: String?,
+    val author: String? = null,
     val link: String,
     val assets: List<String>,
     val category: String,
     val datasource: String,
-    val logo: String?,
+    val logo: String? = null,
     val source: String? = null
 )
