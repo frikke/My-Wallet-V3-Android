1436984657325bcf52464518c813ae0f7c01d93c
Othman El Jazouli <oteljazouli@blockchain.com>
2023-07-17 15:41:29 +0200

    [GROWUX-1243 swap confirmation fix]

--- flowtransactions/presentation/src/main/java/com/blockchain/transactions/sell/enteramount/composable/SellInputErrorScreen.kt
+++ flowtransactions/presentation/src/main/java/com/blockchain/transactions/sell/enteramount/composable/SellInputErrorScreen.kt
@@ -1,10 +1,8 @@
 package com.blockchain.transactions.sell.enteramount.composable
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,6 +12,7 @@ import com.blockchain.componentlib.basic.ComposeGravities
 import com.blockchain.componentlib.basic.ComposeTypographies
 import com.blockchain.componentlib.basic.SimpleText
 import com.blockchain.componentlib.sheets.SheetHeader
+import com.blockchain.componentlib.theme.AppColors
 import com.blockchain.componentlib.theme.AppTheme
 import com.blockchain.componentlib.theme.SmallVerticalSpacer
 import com.blockchain.componentlib.theme.SmallestVerticalSpacer
@@ -25,24 +24,30 @@ fun SellInputErrorScreen(
     inputError: SellEnterAmountInputError,
     closeClicked: () -> Unit,
 ) {
-    val navBarHeight = WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding()
-
     Column(
         Modifier
             .fillMaxWidth()
-            .padding(bottom = navBarHeight)
+            .background(AppColors.background)
     ) {
-        SheetHeader(onClosePress = closeClicked, shouldShowDivider = false)
+        SheetHeader(
+            onClosePress = closeClicked,
+            shouldShowDivider = false,
+            backgroundSecondary = false
+        )
 
         val title = when (inputError) {
             is SellEnterAmountInputError.AboveBalance ->
                 stringResource(R.string.not_enough_funds, inputError.displayTicker)
+
             is SellEnterAmountInputError.AboveMaximum ->
                 stringResource(R.string.maximum_with_value, inputError.maxValue)
+
             is SellEnterAmountInputError.BelowMinimum ->
                 stringResource(R.string.minimum_with_value, inputError.minValue)
+
             is SellEnterAmountInputError.InsufficientGas ->
                 stringResource(R.string.confirm_status_msg_insufficient_gas, inputError.displayTicker)
+
             is SellEnterAmountInputError.Unknown ->
                 stringResource(R.string.common_error)
         }
@@ -54,12 +59,16 @@ fun SellInputErrorScreen(
                 "sell",
                 inputError.balance,
             )
+
             is SellEnterAmountInputError.AboveMaximum ->
                 stringResource(R.string.trading_amount_above_max, inputError.maxValue)
+
             is SellEnterAmountInputError.BelowMinimum ->
                 stringResource(R.string.minimum_sell_error_message, inputError.minValue)
+
             is SellEnterAmountInputError.InsufficientGas ->
                 stringResource(R.string.confirm_status_msg_insufficient_gas_description, inputError.networkName)
+
             is SellEnterAmountInputError.Unknown ->
                 inputError.error ?: stringResource(R.string.common_error)
         }
@@ -80,7 +89,7 @@ fun SellInputErrorScreen(
                 .padding(horizontal = AppTheme.dimensions.smallSpacing),
             text = description,
             style = ComposeTypographies.Paragraph1,
-            color = ComposeColors.Title,
+            color = ComposeColors.Body,
             gravity = ComposeGravities.Start,
         )
         SmallVerticalSpacer()
