8f6715a6cd170aee8065886f1de5610546fcb729
Othman El Jazouli <oteljazouli@blockchain.com>
2023-07-19 14:08:27 +0200

    [GROWUX-1316 Fix Earn Quick Actions] kt

--- earn/presentation/src/main/java/com/blockchain/earn/staking/StakingSummarySheet.kt
+++ earn/presentation/src/main/java/com/blockchain/earn/staking/StakingSummarySheet.kt
@@ -2,7 +2,6 @@ package com.blockchain.earn.staking
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
