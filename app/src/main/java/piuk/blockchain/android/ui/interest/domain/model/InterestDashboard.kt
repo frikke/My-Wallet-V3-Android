21521ae7d93c9b04e5294ebc03a12efe9709aeae
Othman El Jazouli <oteljazouli@blockchain.com>
2023-07-19 18:49:57 +0200

    [GROWUX-1320-kill old color palette] rm old interest dashboard

--- app/src/main/java/piuk/blockchain/android/ui/interest/domain/model/InterestDashboard.kt
+++
@@ -1,9 +0,0 @@
-package piuk.blockchain.android.ui.interest.domain.model
-
-import com.blockchain.core.kyc.domain.model.KycTiers
-import info.blockchain.balance.AssetInfo
-
-data class InterestDashboard(
-    val tiers: KycTiers,
-    val enabledAssets: List<AssetInfo>
-)
