825ae4452ca1cb6eea9c126f243720dffd0d12ca
Othman El Jazouli <oteljazouli@blockchain.com>
2023-07-20 17:02:07 +0200

    [GROWUX-1316 Fix staking btns] fix "no balance" sheet when all balance is in earn, i.e. no balance in trading or defi account

--- earn/domain/src/main/java/com/blockchain/earn/domain/models/staking/StakingAccountBalance.kt
+++ earn/domain/src/main/java/com/blockchain/earn/domain/models/staking/StakingAccountBalance.kt
@@ -1,15 +1,16 @@
 package com.blockchain.earn.domain.models.staking
 
+import com.blockchain.earn.domain.models.EarnAccountBalance
 import info.blockchain.balance.Currency
 import info.blockchain.balance.Money
 
 data class StakingAccountBalance(
-    val totalBalance: Money,
+    override val totalBalance: Money,
     val lockedBalance: Money,
     val pendingDeposit: Money,
     val pendingWithdrawal: Money,
     val totalRewards: Money
-) {
+) : EarnAccountBalance {
     val availableBalance = totalBalance - lockedBalance - pendingWithdrawal - pendingDeposit
 
     companion object {
